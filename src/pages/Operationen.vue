87495a044bed898bb35932dc46fc103f5239007d
deepsoul <bhorn@netfonds.de>
2025-07-03 10:57:19 +0200

    Weitere UI/UX- oder Inhaltsanpassungen (Operationen, style.css)

--- src/pages/Operationen.vue
+++ src/pages/Operationen.vue
@@ -2,25 +2,47 @@
   <v-container class="my-2 my-md-12">
     <h2 class="font-weight-bold mt-0 mb-md-8 text-center">Operationen</h2>
     <v-card class="pa-6 mx-auto" elevation="2" max-width="600">
-      <v-list density="compact">
-        <v-list-item
-          v-for="item in operationen"
-          :key="item"
-          :title="item"
-          prepend-icon="mdi-doctor"
-        />
-      </v-list>
+      <v-card-text>
+        <p class="pb-2">
+          Manche Erkrankungen erfordern zur kompletten Abheilung einen
+          Operativen Eingriff.
+        </p>
+
+        <p class="pb-2">
+          Wir führen unsere Operationen –<em>je nach Art des Eingriffe</em>s-
+          sowohl in örtlicher Betäubung als auch in Kurznarkose durch.
+        </p>
+
+        <p class="pb-2">
+          Die ambulanten Operationen in Kurznarkose werden in unseren
+          Praxisräumlichkeiten durchgeführt. Hierzu steht uns ein entsprechender
+          Operationssaal zur Verfügung. Die hierfür notwendige Narkose wird von
+          sehr erfahrenen Anästhesisten durchgeführt. Die entsprechenden Details
+          erfahren Sie bei dem ausführlichen vorbereitenden Gespräch.
+        </p>
+
+        <p class="pb-2">
+          Informationen über den Ablauf der Narkose finden Sie unter:
+        </p>
+
+        <p class="pb-2">
+          <strong>Praxis für Anästhesie, Schmerztherapie und Akupunktur</strong>
+        </p>
+
+        <p class="pb-2">
+          Bahnhofplatz 1<br />74321 Bietigheim-Bissingen<br />Telefon
+          07142-64000
+        </p>
+
+        <p class="pb-2">
+          <a
+            href="https://www.arzt-narkose.de"
+            target="_blank"
+            rel="noreferrer noopener"
+            >https://www.arzt-narkose.de</a
+          >
+        </p>
+      </v-card-text>
     </v-card>
   </v-container>
 </template>
-<script setup>
-const operationen = [
-  'Hämorrhoidenoperation',
-  'Analfissuren und Abszesse',
-  'Analfistel',
-  'Condylomabtragung',
-  'Sinus pilonidalis (Steißbeinfistel)',
-  'Mariskenabtragung',
-  'Polyp/Tumorentfernung über den After (transanale Polypexzision)',
-];
-</script>
